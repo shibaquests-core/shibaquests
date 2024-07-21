ce0d18bcdbbac20ccedeaa39034ecdb8ded08768
strandgeek <strandgeek@gmail.com>
2024-07-21 07:10:07 +0000

    feat(webapp): setup Web3Modal
    Changes:
    
    * Integration with Web3Modal

--- webapp/src/main.tsx
+++ webapp/src/main.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Web3ModalProvider } from "./providers/Web3ModalProvider";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
@@ -19,13 +20,15 @@ const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ContractContextProvider>
-      <ToastifyProvider>
-        <QueryClientProvider client={queryClient}>
-          <RouterProvider router={router} />
-          <ReactQueryDevtools initialIsOpen={false} />
-        </QueryClientProvider>
-      </ToastifyProvider>
-    </ContractContextProvider>
+    <Web3ModalProvider>
+      <ContractContextProvider>
+        <ToastifyProvider>
+          <QueryClientProvider client={queryClient}>
+            <RouterProvider router={router} />
+            <ReactQueryDevtools initialIsOpen={false} />
+          </QueryClientProvider>
+        </ToastifyProvider>
+      </ContractContextProvider>
+    </Web3ModalProvider>
   </React.StrictMode>,
 );
