b95b898b8b0bd5914d336a752928ce3e7ababf01
strandgeek <strandgeek@gmail.com>
2024-07-21 10:30:59 +0000

    feat(contracts): setup hardhat-abi-exporter
    Changes:
    
    * Configure build script for contracts
    * Setup hardhat-abi-exporter

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,8 +1,13 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import 'hardhat-abi-exporter';
 
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
+  abiExporter: {
+    path: '../webapp/src/abi',
+    flat: true,
+  },
 };
 
 export default config;
