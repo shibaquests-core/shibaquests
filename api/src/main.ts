2e7fa68787d37fa91515a3bb4982bcdc17abbe15
strandgeek <strandgeek@gmail.com>
2024-07-21 09:31:30 +0000

    chore(api): setup swagger
    Changes:
    
    * Install @nestjs/swagger
    * Configure API documentation

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,24 @@
+// LIB IMPORTS - START
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+// LIB IMPORTS - END
+
+// INTERNAL IMPORTS - START
 import { AppModule } from './app.module';
+// INTERNAL IMPORTS - END
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // APP SETUP - START
+  const config = new DocumentBuilder()
+    .setTitle('MyProject API Documentation')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  // APP SETUP - END
+
   await app.listen(3000);
 }
 bootstrap();
