a74800500706709ada204c93494fe788023838da
strandgeek <strandgeek@gmail.com>
2024-07-21 09:17:19 +0000

    chore(api): initial api project
    Changes:
    
    * Implement initial setup using NestJS
    * Setup initial ESLint & Typescript configuration

---
+++ api/src/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+
+// IMPORTS - START
+// IMPORTS - END
+
+@Module({
+  imports: [
+    // MODULE IMPORTS - START
+    // MODULE IMPORTS - END
+  ],
+  controllers: [
+    // MODULE CONTROLLERS - START
+    // MODULE CONTROLLERS - END
+  ],
+  providers: [
+    // MODULE PROVIDERS - START
+    // MODULE PROVIDERS - END
+  ],
+})
+export class AppModule {}
