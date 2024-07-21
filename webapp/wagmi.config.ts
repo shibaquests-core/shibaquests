a65717821b0ce118fb51febf18bb939e3f434eec
strandgeek <strandgeek@gmail.com>
2024-07-21 07:42:28 +0000

    feat(webapp): add factory contract to wagmi-cli
    Changes:
    
    * Add the factory contract to wagmi-cli config

--- webapp/wagmi.config.ts
+++ webapp/wagmi.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from '@wagmi/cli';
 import { react } from '@wagmi/cli/plugins';
 import SomeExample from './src/abi/SomeExample.json';
+import SomeExampleFactory from './src/abi/SomeExampleFactory.json';
  
 export default defineConfig({
   out: 'src/generated.ts',
@@ -8,6 +9,10 @@ export default defineConfig({
     {
       name: 'SomeExample',
       abi: SomeExample,
+    },
+    {
+      name: 'SomeExampleFactory',
+      abi: SomeExampleFactory,
     }
   ],
   plugins: [
