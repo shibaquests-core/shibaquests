a24a366b77a058ce322f76f2332e26110335c698
strandgeek <strandgeek@gmail.com>
2024-08-17 02:10:39 +0000

    feat(webapp): add NetworkLabel component
    Changes:
    
    * Add NetworkLabel component

---
+++ webapp/src/components/NetworkLabel.tsx
@@ -0,0 +1,21 @@
+import React, { FC } from 'react'
+
+export interface NetworkLabelProps {
+  color?: string
+  name?: string
+}
+
+export const NetworkLabel: FC<NetworkLabelProps> = ({
+  name = 'Unknown',
+  color = '#BBC0C4',
+}) => {
+  return (
+    <div>
+    <span
+      className="inline-block w-2 h-2 rounded-full mr-1"
+      style={{ backgroundColor: color }}
+    />
+      <span className="opacity-60 text-xs">{name}</span>
+    </div>
+  );
+}
